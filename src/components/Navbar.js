25664ec35c87a47a6eb059becd7fb3ed27020f34
rbanda1985 <robertbanda85@gmail.com>
2022-04-27 11:48:20 -0500

    ADD: Navbar component with styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+import './Navbar.css'
+
+const Navbar = () => {
+  const [showNavCentred, setShowNavCentred] = useState(false);
+  return(
+    <>
+      <MDBNavbar expand='lg' light bgColor='dark' className='navigation-bar'>
+      <MDBContainer fluid>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarCenteredExample'
+          aria-controls='navbarCenteredExample'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavCentred(!showNavCentred)}
+          style={{color: 'white'}}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showNavCentred} center id='navbarCenteredExample'>
+          <MDBNavbarNav fullWidth={false} className='mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+               <span className='navigation-link'>Home</span>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>
+              <span className='navigation-link'>Stylist</span>
+                </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>
+              <span className='navigation-link'>Policies</span>
+                </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>
+              <span className='navigation-link'>Contact</span>
+                </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>
+              <span className='navigation-link'>Gallery</span>
+                </MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+    </>
+  )
+}
+
+
+
+export default Navbar;
