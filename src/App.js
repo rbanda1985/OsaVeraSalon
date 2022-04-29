d21a8f3f890c2a2efa1187bc371bcb70829677e3
rbanda1985 <robertbanda85@gmail.com>
2022-04-29 06:40:03 -0500

    ADD: Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import SalonName from './components/SalonName';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SalonName />
+      <Footer />
     </div>
   );
 }
