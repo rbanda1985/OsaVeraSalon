81c1256baa8c6f83eecad67ca91e5b21f5869cd6
rbanda1985 <robertbanda85@gmail.com>
2022-04-29 05:49:18 -0500

    ADD: Salon name comonent with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import SalonName from './components/SalonName';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SalonName />
     </div>
   );
 }
---
+++ src/components/SalonName.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './SalonName.css'
+
+const SalonName = () => {
+  return (
+    <div className='name-container'>
+      <div className='salon-one'>
+      <span>O</span>
+      <span>s</span>
+      <span>a</span>
+      <span>V</span>
+      <span>e</span>
+      <span>r</span>
+      <span>a</span>
+      </div>
+      <div className='salon-two'>
+      <span>S</span>
+      <span>a</span>
+      <span>l</span>
+      <span>o</span>
+      <span>n</span>
+      </div>
+    </div>
+  )
+}
+
+export default SalonName
